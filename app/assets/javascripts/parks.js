9f0fccdcdd49298105b8d19d0f51b77f22ff9e6a
Moe Sunami <17sunamim@gmail.com>
2014-03-23 13:54:27 +0900

    asdfasdfasdfa

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -59,3 +59,124 @@ $(function() {
   "controller" => "parks"
 }
 */
+
+/* ===================================================================
+
+ * スムーススクロール
+
+=================================================================== */
+$(function(){
+   // #で始まるアンカーをクリックした場合に処理
+   $('a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 400;// ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top;
+      // スムーススクロール
+      $($.browser.safari ? 'body' : 'html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+   });
+});
+
+
+
+// アイコン切り替え、mosueover
+
+icon1 = new Image();
+icon1.src = "flower-icon-blown.png";
+ function brown1(){document.icon1.src="flower-icon-brown.png";}
+ function white1(){document.icon1.src="flower-icon-white.png";}
+
+icon2 = new Image();
+icon2.src = "toilet-icon-blown.png";
+ function brown2(){document.icon2.src="toilet-icon-brown.png";}
+ function white2(){document.icon2.src="toilet-icon-white.png";}
+
+icon3 = new Image();
+icon3.src = "water-icon-blown.png";
+ function brown3(){document.icon3.src="water-icon-brown.png";}
+ function white3(){document.icon3.src="water-icon-white.png";}
+
+icon4 = new Image();
+icon4.src = "bench-icon-blown.png";
+ function brown4(){document.icon4.src="bench-icon-brown.png";}
+ function white4(){document.icon4.src="bench-icon-white.png";}
+
+icon5 = new Image();
+icon5.src = "parking-icon-blown.png";
+ function brown5(){document.icon5.src="parking-icon-brown.png";}
+ function white5(){document.icon5.src="parking-icon-white.png";}
+
+icon6 = new Image();
+icon6.src = "sports-icon-blown.png";
+ function brown6(){document.icon6.src="sports-icon-brown.png";}
+ function white6(){document.icon6.src="sports-icon-white.png";}
+
+icon7 = new Image();
+icon7.src = "playground-icon-blown.png";
+ function brown7(){document.icon7.src="playground-icon-brown.png";}
+ function white7(){document.icon7.src="playground-icon-white.png";}
+
+
+//onclick---icon underline
+
+function closePopUp() {
+  fukidashi.style.display = "none";
+}
+function openPopUp() {
+  closePopUp();
+  fukidashi.style.display = "block";
+}
+
+function closePopUp2() {
+  fukidashi2.style.display = "none";
+}
+function openPopUp2() {
+  closePopUp();
+  fukidashi2.style.display = "block";
+}
+
+function closePopUp3() {
+  fukidashi3.style.display = "none";
+}
+function openPopUp3() {
+  closePopUp();
+  fukidashi3.style.display = "block";
+}
+
+function closePopUp4() {
+  fukidashi4.style.display = "none";
+}
+function openPopUp4() {
+  closePopUp();
+  fukidashi4.style.display = "block";
+}
+
+function closePopUp5() {
+  fukidashi5.style.display = "none";
+}
+function openPopUp5() {
+  closePopUp();
+  fukidashi5.style.display = "block";
+}
+
+function closePopUp6() {
+  fukidashi6.style.display = "none";
+}
+function openPopUp6() {
+  closePopUp();
+  fukidashi6.style.display = "block";
+}
+
+function closePopUp7() {
+  fukidashi7.style.display = "none";
+}
+function openPopUp7() {
+  closePopUp();
+  fukidashi7.style.display = "block";
+}
+
